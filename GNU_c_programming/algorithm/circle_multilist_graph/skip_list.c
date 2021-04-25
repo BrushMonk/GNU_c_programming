3723bf0ceb04f9aa2ac9860703780d5fab9cb799
BrushMonk <brushmonk@qq.com>
2021-04-25 13:06:55 +0800

    commit for brushmonk_repository

--- GNU_c_programming/algorithm/circle_multilist_graph/skip_list.c
+++ GNU_c_programming/algorithm/circle_multilist_graph/skip_list.c
@@ -5,7 +5,7 @@
 #include <time.h>
 struct skip_list_node {
     int32_t key_value;
-    int *file_dscrptor;
+    int *fd;
     struct skip_list_node **next;};
 
 void init_skip_list(struct skip_list_node **head_to_skip_list, size_t n)
